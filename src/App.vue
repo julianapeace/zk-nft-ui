c89a5c14828e48310c33ef8b618fd4b7ebb176c9
julianapeace <chancecordelia@gmail.com>
2022-02-19 12:59:10 -0700

    not compiling, trying to create account

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import { Strategy, ZkIdentity } from "@zk-kit/identity"
+import { Semaphore } from "@zk-kit/protocols"
+import HelloWorld from './components/HelloWorld.vue'
+
+
+try {
+  const identity = new ZkIdentity()
+  const identityCommitment = identity.genIdentityCommitment()
+  console.log('============================================');
+  console.log('============================================');
+  console.log(identityCommitment);
+} catch (err) {
+  console.log(err);
+}
+
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
